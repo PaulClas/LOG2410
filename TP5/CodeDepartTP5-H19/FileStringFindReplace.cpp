e1210c8dcc16f21fb25d849a718fb353a64beea9
PaulClas <paul.clas@polymtl.ca>
2019-04-15 19:26:13 -0400

    File String completé

--- TP5/CodeDepartTP5-H19/FileStringFindReplace.cpp
+++ TP5/CodeDepartTP5-H19/FileStringFindReplace.cpp
@@ -23,10 +23,16 @@ void FileStringFindReplace::visit(const AudioFile & f)
 {
 	// A COMPLETER:
 	// Iterer sur chaque Chunk du fichier d'entree
-	//    - Construire un nouvel iterateur pointant a la fin du fichier de sortie
-	//    - Initialiser la memoire de l'iterateur avec le contenu du Chunk d'entree
-	//    - Remplacer les caracteres dans le Chunk de sortie avec findReplace
-	//    - Ajouter le nouveau Chunk au fichier de sortie
+	for (auto it = f.begin(); it != f.end(); it++) {
+		//    - Construire un nouvel iterateur pointant a la fin du fichier de sortie
+		auto itEnd = m_outFile->end();
+		//    - Initialiser la memoire de l'iterateur avec le contenu du Chunk d'entree
+		*itEnd = *it;
+		//    - Remplacer les caracteres dans le Chunk de sortie avec findReplace
+		findReplace(itEnd->get(), itEnd->get() + itEnd->size());
+		//    - Ajouter le nouveau Chunk au fichier de sortie
+		m_outFile->addChunk(itEnd);
+	}
 }
 
 void FileStringFindReplace::visit(const MemAudioFile & f)
@@ -36,11 +42,18 @@ void FileStringFindReplace::visit(const MemAudioFile & f)
 	const char* buf = f.get() + f.getHeaderSize();
 	// A COMPLETER:
 	// Iterer sur chaque Chunk du fichier d'entree dans buf
-	//    - Construire un nouvel iterateur pointant a la fin du fichier de sortie
-	//    - Initialiser la memoire de l'iterateur avec le contenu du Chunk d'entree
-	//    - Remplacer les caracteres dans le Chunk de sortie avec findReplace
-	//    - Ajouter le nouveau Chunk au fichier de sortie
-	//    - Avancer dans buf
+	for (auto it = f.begin(); it != f.end(); it++) {
+		//    - Construire un nouvel iterateur pointant a la fin du fichier de sortie
+		auto itEnd = m_outFile->end();
+		//    - Initialiser la memoire de l'iterateur avec le contenu du Chunk d'entree
+		*itEnd = *it;
+		//    - Remplacer les caracteres dans le Chunk de sortie avec findReplace
+		findReplace(itEnd->get(), itEnd->get() + itEnd->size());
+		//    - Ajouter le nouveau Chunk au fichier de sortie
+		m_outFile->addChunk(itEnd);
+		//    - Avancer dans buf
+		buf++;
+	}
 }
 
 void FileStringFindReplace::findReplace(char * p_beg, char * p_end)

6bd771489181d02a4116add44b8f87dcf1371f30
PaulClas <paul.clas@polymtl.ca>
2019-04-15 19:34:47 -0400

    aide a la remise

---
+++ TP5/aide/LOG2410_TP5_1929587_1932805/ChecksumCommand.cpp
@@ -0,0 +1,9 @@
+#include "ChecksumCommand.h"
+#include "AbsAudioFile.h"
+
+void ChecksumCommand::execute(AbsAudioFile & f)
+{
+	// Declencher l'execution de la commande
+	// A COMPLETER...
+	f.accept(m_visitor);
+}
---
+++ TP5/aide/LOG2410_TP5_1929587_1932805/StringFindReplaceCommand.cpp
@@ -0,0 +1,9 @@
+#include "StringFindReplaceCommand.h"
+#include "AbsAudioFile.h"
+
+void StringFindReplaceCommand::execute(AbsAudioFile & f)
+{
+	// Declencher l'execution de la commande
+	// A COMPLETER...
+	f.accept(m_visitor);
+}
